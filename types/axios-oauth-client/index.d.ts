6f4a1faae04db643bd31d46d28cafd55e0e8a4b9
A. Lloyd Flanagan (he/him) <lloyd.flanagan@mobelux.com>
2022-05-05 10:47:32 -0400

    add types for axios-oauth-client. needs tests

---
+++ types/axios-oauth-client/index.d.ts
@@ -0,0 +1,47 @@
+// Type definitions for ZeroMQ Node
+// Project: https://github.com/JustinTulloss/zeromq.node
+// Definitions by: Dave McKeown <https://github.com/davemckeown>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import { AxiosInstance } from 'axios'
+import AxiosTokenProvider from 'axios-token-interceptor'
+
+type ClientOpts = {
+  url: string
+  [key: string]: string
+}
+
+// https://www.rfc-editor.org/rfc/rfc6749#section-4.1.2.1
+type APICallErrorData = {
+  error:
+    | 'invalid_request'
+    | 'unauthorized_client'
+    | 'access_denied'
+    | 'unsupported_response_type'
+    | 'invalid_scope'
+    | 'server_error'
+    | 'temporarily_unavailable'
+  error_description?: string
+  error_uri?: string
+  state?: string // REQUIRED if caller sent it
+}
+
+// see https://www.rfc-editor.org/rfc/rfc6749#section-4.1.4
+type AccessTokenResponseData = {
+  access_token: string
+  expires_in: number
+  refresh_token?: string
+  [key: string]: string | number | undefined
+}
+
+export function client(
+  axios: AxiosInstance,
+  options: ClientOpts
+): () => Promise<AccessTokenResponseData | APICallErrorData>
+
+export function interceptor(
+  tokenProvider: AxiosTokenProvider.TokenProvider,
+  authenticate: any
+): AxiosTokenProvider.TokenProvider
