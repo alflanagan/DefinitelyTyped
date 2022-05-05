baa562b3a806492cb59e59b65cd19bdd4b1d77b4
A. Lloyd Flanagan (he/him) <lloyd.flanagan@mobelux.com>
2022-05-05 13:16:46 -0400

    fix headers, clean up for tslint

--- types/axios-oauth-client/index.d.ts
+++ types/axios-oauth-client/index.d.ts
@@ -1,55 +1,47 @@
-// Type definitions for ZeroMQ Node
-// Project: https://github.com/JustinTulloss/zeromq.node
-// Definitions by: Dave McKeown <https://github.com/davemckeown>
+// Type definitions for axios-oauth-client 1.4
+// Project: https://github.com/compwright/axios-oauth-client
+// Definitions by: Adrian Flanagan <https://github.com/alflanagan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript version 4.7
 
 /// <reference types="node" />
+import { AxiosInstance } from 'axios';
+import AxiosTokenProvider = require('axios-token-interceptor');
 
-import { AxiosInstance } from 'axios'
-import AxiosTokenProvider from 'axios-token-interceptor'
-
-type ClientOpts = {
-  url: string
-  [key: string]: string
+export interface ClientOpts {
+    url: string;
+    [key: string]: string;
 }
 
 // https://www.rfc-editor.org/rfc/rfc6749#section-4.1.2.1
-type APICallErrorData = {
-  error:
-    | 'invalid_request'
-    | 'unauthorized_client'
-    | 'access_denied'
-    | 'unsupported_response_type'
-    | 'invalid_scope'
-    | 'server_error'
-    | 'temporarily_unavailable'
-  error_description?: string
-  error_uri?: string
-  state?: string // REQUIRED if caller sent it
+export interface APICallErrorData {
+    error:
+        | 'invalid_request'
+        | 'unauthorized_client'
+        | 'access_denied'
+        | 'unsupported_response_type'
+        | 'invalid_scope'
+        | 'server_error'
+        | 'temporarily_unavailable';
+    error_description?: string;
+    error_uri?: string;
+    state?: string; // REQUIRED if caller sent it
 }
 
 // see https://www.rfc-editor.org/rfc/rfc6749#section-4.1.4
-type AccessTokenResponseData = {
-  access_token: string
-  expires_in: number
-  refresh_token?: string
-  [key: string]: string | number | undefined
+export interface AccessTokenResponseData {
+    access_token: string;
+    expires_in: number;
+    refresh_token?: string;
+    [key: string]: string | number | undefined;
 }
 
-declare function client(
-  axios: AxiosInstance,
-  options: ClientOpts
-): () => Promise<AccessTokenResponseData | APICallErrorData>
+export function client(
+    axios: AxiosInstance,
+    options: ClientOpts,
+): () => Promise<AccessTokenResponseData | APICallErrorData>;
 
-declare function interceptor(
-  tokenProvider: AxiosTokenProvider.TokenProvider,
-  authenticate: any
-): AxiosTokenProvider.TokenProvider
-
-export {
-    client,
-    interceptor,
-    APICallErrorData,
-    AccessTokenResponseData,
-    ClientOpts
-}
+export function interceptor(
+    tokenProvider: AxiosTokenProvider.TokenProvider,
+    authenticate: any,
+): AxiosTokenProvider.TokenProvider;
